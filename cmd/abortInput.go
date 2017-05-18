08cd263c1c0fc6321f251d3cbc9540c9b9783d2a
mattddowney <mattddowney@gmail.com>
2017-05-17 23:30:44 -0600

    remove init comment boilerplate

--- cmd/abortInput.go
+++ cmd/abortInput.go
@@ -62,14 +62,4 @@ var abortInputCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(abortInputCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// abortInputCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// abortInputCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
