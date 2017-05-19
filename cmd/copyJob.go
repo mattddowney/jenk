e6123afe78fd2bf614fc0b539addd6eb9f4c182b
mattddowney <mattddowney@gmail.com>
2017-05-18 19:18:01 -0600

    implement copy-job

--- cmd/copyJob.go
+++ cmd/copyJob.go
@@ -6,7 +6,9 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"net/url"
 
+	"github.com/mattddowney/jenk/jenkins"
 	"github.com/spf13/cobra"
 )
 
@@ -24,6 +26,27 @@ var copyJobCmd = &cobra.Command{
 			fmt.Printf("Command:\tcopy-job\n")
 			fmt.Printf("From Job Name:\t%s\n", fromJobName)
 			fmt.Printf("New Job Name:\t%s\n", newJobName)
+
+			// build url
+			reqURL := "/createItem?name=" + newJobName + "&mode=copy&from=" + fromJobName
+
+			// create body
+			body := url.Values{}
+			body.Set("json", "{\"parameter\": []}")
+
+			// issue the request
+			statusCode, status, _, err := jenkins.Request("POST", reqURL, &body)
+			if err != nil {
+				return err
+			}
+
+			// log status
+			fmt.Printf("StatusCode:\t%d\n", statusCode)
+			fmt.Printf("Status:\t\t%s\n", status)
+
+			if statusCode != 200 {
+				return errors.New("Request failed")
+			}
 		} else {
 			return errors.New("<from_job_name>, <new_job_name> required")
 		}
