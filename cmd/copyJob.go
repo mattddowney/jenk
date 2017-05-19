428e759408ef0a53bc18e4e0a7e06b6452716ff3
mattddowney <mattddowney@gmail.com>
2017-05-18 18:54:14 -0600

    require arguments and err if not

--- cmd/copyJob.go
+++ cmd/copyJob.go
@@ -4,6 +4,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -14,8 +15,20 @@ var copyJobCmd = &cobra.Command{
 	Use:   "copy-job",
 	Short: "Copy a Jenkins job",
 
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("copyJob called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 2 {
+			var fromJobName = args[0]
+			var newJobName = args[1]
+
+			// log
+			fmt.Printf("Command:\tcopy-job\n")
+			fmt.Printf("From Job Name:\t%s\n", fromJobName)
+			fmt.Printf("New Job Name:\t%s\n", newJobName)
+		} else {
+			return errors.New("<from_job_name>, <new_job_name> required")
+		}
+
+		return nil
 	},
 }
 
